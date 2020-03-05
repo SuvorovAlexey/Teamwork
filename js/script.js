361b9047418bc9c76752da8d8aaccf9c4abb2676
Suvorov Aleksey <suvorov@hostco.ru>
2020-03-05 18:06:08 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let btn = document.querySelectorAll('.btns__item')
+let content = document.querySelectorAll('.content__item')
+
+//функция показа контента по нажатию кнопки
+let showContent = (props) => {
+
+  let btnActive = document.querySelector('.btn-active')
+  btnActive.classList.remove('btn-active')
+  btn[props].classList.add('btn-active')
+
+  let contentActive = document.querySelector('.content-active')
+  contentActive.classList.remove('content-active')
+
+  content[props].classList.add('content-active')
+}
+
+//к кнопкам применяем функцию
+btn[0].addEventListener('click', function(){showContent(0)})
+btn[1].addEventListener('click', function(){showContent(1)})
+btn[2].addEventListener('click', function(){showContent(2)})
+btn[3].addEventListener('click', function(){showContent(3)})
+btn[4].addEventListener('click', function(){showContent(4)})
+
